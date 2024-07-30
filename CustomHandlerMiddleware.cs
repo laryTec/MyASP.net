62caf9d1e59669bc29f00ca25ac31beef5a1ee2c
lyhuang <104208098@g.nccu.edu.tw>
2024-07-31 00:39:29 +0800

    --init

---
+++ CustomHandlerMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MyFirstASP.net
+{
+    public class CustomHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path.Equals("/handler.ashx"))
+            {
+                // 用POST
+                if (context.Request.Method == "POST")
+                {
+                    var uname = context.Request.Form["uname"];
+                    var upwd = context.Request.Form["upwd"];
+
+                    // 驗證測試
+                    if (uname == "admin" && upwd == "password")
+                    {
+                        context.Response.ContentType = "text/plain";
+                        await context.Response.WriteAsync("susses");
+                    }
+                    else
+                    {
+                        context.Response.ContentType = "text/plain";
+                        await context.Response.WriteAsync("wrong");
+                    }
+                }
+                else
+                {
+                    // 如果不是 POST RETUEN 405
+                    context.Response.StatusCode = StatusCodes.Status405MethodNotAllowed;
+                }
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyFirstASP.net; // 引用 CustomHandlerMiddleware 的命名
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 安裝配置
+builder.Services.AddDirectoryBrowser();
+
+var app = builder.Build();
+
+// 自定義中間件
+app.UseMiddleware<CustomHandlerMiddleware>();
+
+// 使用靜態文件
+app.UseStaticFiles();
+app.UseDirectoryBrowser();
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
